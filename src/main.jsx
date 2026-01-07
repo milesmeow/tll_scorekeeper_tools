35a68d3b6c6934275b1d3883d3a1d2d0732618e8
milesmeow <176552+milesmeow@users.noreply.github.com>
2026-01-06 20:33:04 -0800

    updates import

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights />
     <App />
+    <SpeedInsights />
   </React.StrictMode>,
 )
